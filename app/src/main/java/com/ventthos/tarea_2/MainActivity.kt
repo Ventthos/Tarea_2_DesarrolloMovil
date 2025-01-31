6e50a685e708f19e1d208bfa81078918566ab538
chaan12 <eduardoezequielchanortiz@gmail.com>
2025-01-31 01:14:32 -0600

    ToggleButtom y CheckBox
    Ya estan las dos funciones, el Toggle, al darle click muestra una lista de "extras" y las oculta al darle click de nuevo.

--- app/src/main/java/com/ventthos/tarea_2/MainActivity.kt
+++ app/src/main/java/com/ventthos/tarea_2/MainActivity.kt
@@ -3,14 +3,53 @@ package com.ventthos.tarea_2
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.*
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val checkExtraCheese = findViewById<CheckBox>(R.id.checkExtraCheese)
+        val checkMushrooms = findViewById<CheckBox>(R.id.checkMushrooms)
+        val checkBacon = findViewById<CheckBox>(R.id.checkBacon)
+        val toggleExtras = findViewById<ToggleButton>(R.id.toggleExtras)
+        val extrasContainer = findViewById<LinearLayout>(R.id.extrasContainer)
+        val textSelected = findViewById<TextView>(R.id.textSelected)
+
+        extrasContainer.visibility = View.GONE
+
+        toggleExtras.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                extrasContainer.visibility = View.VISIBLE
+            } else {
+                extrasContainer.visibility = View.GONE
+            }
+        }
+
+        fun actualizarSeleccion() {
+            var extras = ""
+            if (checkExtraCheese.isChecked) {
+                extras += getString(R.string.extra_cheese) + ", "
+            }
+            if (checkMushrooms.isChecked) {
+                extras += getString(R.string.mushrooms) + ", "
+            }
+            if (checkBacon.isChecked) {
+                extras += getString(R.string.bacon) + ", "
+            }
+            if (extras.isNotEmpty()) {
+                extras = extras.dropLast(2)
+            }
+            textSelected.text = getString(R.string.extras) + "$extras"
+        }
+
+        checkExtraCheese.setOnCheckedChangeListener { _, _ -> actualizarSeleccion() }
+        checkMushrooms.setOnCheckedChangeListener { _, _ -> actualizarSeleccion() }
+        checkBacon.setOnCheckedChangeListener { _, _ -> actualizarSeleccion() }
     }
 }
